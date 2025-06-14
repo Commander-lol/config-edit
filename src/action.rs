9f69a27a7fb0f6761d6969e38d3239188cf738de
Louis <contact@louiscap.co>
2025-06-14 22:33:29 +0100

    Lint & format

--- src/action.rs
+++ src/action.rs
@@ -29,39 +29,41 @@ impl ActionDef for Action {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use serde_json::json;
+	use super::*;
+	use serde_json::json;
 
-    #[test]
-    fn test_action_set() {
-        let mut action = Action::Set(add::SetAction {
-            key: "/a".to_string(),
-            value: "42".to_string(),
-        });
-        let input = json!({"a": 1});
-        let expected = json!({"a": 42});
-        let result = action.apply(input).unwrap();
-        assert_eq!(result, expected);
-    }
+	#[test]
+	fn test_action_set() {
+		let mut action = Action::Set(add::SetAction {
+			key: "/a".to_string(),
+			value: "42".to_string(),
+		});
+		let input = json!({"a": 1});
+		let expected = json!({"a": 42});
+		let result = action.apply(input).unwrap();
+		assert_eq!(result, expected);
+	}
 
-    #[test]
-    fn test_action_append() {
-        let mut action = Action::Append(add::AppendAction {
-            key: "/arr".to_string(),
-            value: "42".to_string(),
-        });
-        let input = json!({"arr": [1, 2, 3]});
-        let expected = json!({"arr": [1, 2, 3, 42]});
-        let result = action.apply(input).unwrap();
-        assert_eq!(result, expected);
-    }
+	#[test]
+	fn test_action_append() {
+		let mut action = Action::Append(add::AppendAction {
+			key: "/arr".to_string(),
+			value: "42".to_string(),
+		});
+		let input = json!({"arr": [1, 2, 3]});
+		let expected = json!({"arr": [1, 2, 3, 42]});
+		let result = action.apply(input).unwrap();
+		assert_eq!(result, expected);
+	}
 
-    #[test]
-    fn test_action_get() {
-        let mut action = Action::Get(get::GetAction::new("/a".to_string()));
-        let input = json!({"a": 1});
-        let expected = json!(1);
-        let result = action.apply(input).unwrap();
-        assert_eq!(result, expected);
-    }
+	#[test]
+	fn test_action_get() {
+		let mut action = Action::Get(get::GetAction {
+			key: Some("/a".to_string()),
+		});
+		let input = json!({"a": 1});
+		let expected = json!(1);
+		let result = action.apply(input).unwrap();
+		assert_eq!(result, expected);
+	}
 }
